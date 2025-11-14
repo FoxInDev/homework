04eaf218f0e56ce5c967484d99d8d01eb1ad91de
Даниил <foxy09876@yandex.ru>
2025-11-14 22:21:01 +0300

    fixed tm

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 'use strict';
-
-const now = new Date();
-const newYearDate = new Date(now.getFullYear() + 1, 0, 1);
-const diff = newYearDate - now;
 const timerEl = document.getElementById('timer');
-
-const seconds = Math.floor(diff / 1000) % 60;
-const minutes = Math.floor(diff / (1000 * 60)) % 60;
-const hours = Math.floor(diff / (1000 * 60 * 60)) % 24;
-const totalDays = Math.floor(diff / (1000 * 60 * 60 * 24));
-let months = Math.floor(totalDays / 30);
-const days = totalDays % 30;
-const years = 0;
 
 function updateTimer() {
     timerEl.innerHtml = `${months} месяцев, ${days} дней, ${hours} часов, ${minutes} минут, ${seconds} секунд`;
+    const now = new Date();
+    const newYearDate = new Date(now.getFullYear() + 1, 0, 1);
+    const diff = newYearDate - now;
+
+    const seconds = Math.floor(diff / 1000) % 60;
+    const minutes = Math.floor(diff / (1000 * 60)) % 60;
+    const hours = Math.floor(diff / (1000 * 60 * 60)) % 24;
+    const totalDays = Math.floor(diff / (1000 * 60 * 60 * 24));
+    let months = Math.floor(totalDays / 30);
+    const days = totalDays % 30;
 }
 
 setInterval(updateTimer, 1000);
