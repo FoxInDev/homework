a6cba4b88529d900cefbe41e41999efe18916db0
FoxInDev <programmer.danya.fox@gmail.com>
2025-11-14 22:25:45 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 const timerEl = document.getElementById('timer');
 
 function updateTimer() {
-    timerEl.innerHtml = `${months} месяцев, ${days} дней, ${hours} часов, ${minutes} минут, ${seconds} секунд`;
     const now = new Date();
     const newYearDate = new Date(now.getFullYear() + 1, 0, 1);
     const diff = newYearDate - now;
@@ -13,6 +12,7 @@ function updateTimer() {
     const totalDays = Math.floor(diff / (1000 * 60 * 60 * 24));
     let months = Math.floor(totalDays / 30);
     const days = totalDays % 30;
+    timerEl.innerHTML = `${months} месяцев, ${days} дней, ${hours} часов, ${minutes} минут, ${seconds} секунд`;
 }
 
 setInterval(updateTimer, 1000);
