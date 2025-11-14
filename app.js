3389b7858c3738ed5d901126051935c53faa9249
Даниил <foxy09876@yandex.ru>
2025-11-14 22:17:22 +0300

    tm

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 'use strict';
 
-function calculateBirthday(dateBd) {
-    const now = new Date();
-    let FourteenYearsLater = new Date(dateBd);
-    FourteenYearsLater.setFullYear(FourteenYearsLater.getFullYear() + 14);
+const now = new Date();
+const newYearDate = new Date(now.getFullYear() + 1, 0, 1);
+const diff = newYearDate - now;
+const timerEl = document.getElementById('timer');
 
-    return FourteenYearsLater <= now;
+const seconds = Math.floor(diff / 1000) % 60;
+const minutes = Math.floor(diff / (1000 * 60)) % 60;
+const hours = Math.floor(diff / (1000 * 60 * 60)) % 24;
+const totalDays = Math.floor(diff / (1000 * 60 * 60 * 24));
+let months = Math.floor(totalDays / 30);
+const days = totalDays % 30;
+const years = 0;
+
+function updateTimer() {
+    timerEl.innerHtml = `${months} месяцев, ${days} дней, ${hours} часов, ${minutes} минут, ${seconds} секунд`;
 }
+
+setInterval(updateTimer, 1000);
+updateTimer();
