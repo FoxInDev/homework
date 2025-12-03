2de71eadcb3162b2f3d5c33a0f5fbdf33f019757
Даниил <foxy09876@yandex.ru>
2025-12-03 22:43:40 +0300

    v1

--- app.js
+++ app.js
@@ -1,33 +1,17 @@
 'use strict';
 
-// const req = new XMLHttpRequest();
-// req.open('GET', 'https://pokeapi.co/api/v2/pokemon/ditto');
-// req.send();
-// req.addEventListener("load", function() {
-//     const { abilities } = JSON.parse(this.responseText);
-//     // console.log(abilities[0].ability.url);
-//     const req = new XMLHttpRequest();
-//     req.open('GET', abilities[0].ability.url);
-//     req.send();
+const prom = new Promise((resolve, reject) => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            resolve(position.coords.latitude, position.coords.longitude);
+        })
+    }
+    else {
+        reject(new Error('Ошибка получания координат'));
+    }
+    reject(new Error('Произошла неизвестная ошибка'));
+})
 
-
-//     req.addEventListener('load', function() {
-//         const { effect_entries } = JSON.parse(this.responseText);
-//         console.log(effect_entries[0].effect);
-//     });
-// });
-
-fetch('https://pokeapi.co/api/v2/pokemon/ditto')
-    .then(res => res.json())
-    .then(data => {
-        const UseUrl = data.abilities[0].ability.url;
-        return fetch(UseUrl);
-    })
-    .then(res => res.json())
-    .then(data => {
-        const UseUrl = data.effect_entries[0].effect;
-        console.log(UseUrl)
-    })
-    .catch(err => {
-        throw new Error('ошибка');
-    });
+prom
+    .then(data => console.log(data))
+    .catch(err => console.error(err));
